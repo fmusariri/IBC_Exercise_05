744116c91ed713597faa41126cd36ec95a31657f
Farai Musariri <fmusarir@nd.edu>
2019-10-11 14:14:14 +0200

    Working Soution for Question 1

--- question1.sh
+++ question1.sh
@@ -1,2 +1,6 @@
-sort -k 2n --field-separator=',' wages.csv > sorted_wages.csv
-cut -d "," -f1,2 sorted_wages.csv
+# keep only column 1 and 2 and replace commas with spaces
+cut -d ',' -f 1,2 wages.csv | tr ',' ' ' > sorted_wages.csv
+
+# sort the life according to column 2
+sort -k 2n --field-separator=' ' sorted_wages.csv
+
